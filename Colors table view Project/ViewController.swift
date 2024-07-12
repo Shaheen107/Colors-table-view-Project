1d43700de224a3f2e74a030af7fa2f04d5574239
Shaheen107 <mrizwantariq107@gmail.com>
2024-07-12 23:03:43 +0500

    Initial Commit

---
+++ Colors table view Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Colors table view Project
+//
+//  Created by Apple on 12/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
